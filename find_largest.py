e355d2e7edd9870a44b06b311fb2573545a87533
Gayana Erappa <gayanaerappa2027@gmail.com>
2026-02-12 21:13:28 +0530

    Create find_largest.py

---
+++ find_largest.py
@@ -0,0 +1,12 @@
+def find_largest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+largest = find_largest(num1, num2)
+print("Largest:", largest)
