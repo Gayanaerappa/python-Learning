b2c48f0bd934037762f2fad58bfa28c39dd5be15
Gayana Erappa <gayanaerappa2027@gmail.com>
2026-02-12 21:15:40 +0530

    Create Student_Introduction.py

---
+++ Student_Introduction.py
@@ -0,0 +1,10 @@
+def introduce(name, age, city, course):
+    print("\n--- Student Details ---")
+    print("Name   :", name)
+    print("Age    :", age)
+    print("City   :", city)
+    print("Course :", course)
+
+
+# Calling function using keyword arguments
+introduce(age=22, name="Gayana", course="Data Science", city="Bangalore")
